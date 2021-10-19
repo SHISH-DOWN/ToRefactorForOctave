c0a8e8ae1d844df093ed9fdb54e0b2f4fd8c198a
Mikhail Zhukevich <zhukevich_ma_19@student.grsu.by>
2021-10-19 20:48:07 +0300

    fixed bug with little x0

--- ToRefactorForOctave/Program.cs
+++ ToRefactorForOctave/Program.cs
@@ -14,7 +14,7 @@ namespace ToRefactorForOctave
         public const double G = 9.81;
         public const double density = 1.2;
         public const double Multiplier = 0.5;
-        public const double vel0 = 0;
+        public const double el0 = 0;
         public static double accel;
         public static double spd;
         public static double deltat;
@@ -32,22 +32,23 @@ namespace ToRefactorForOctave
             Console.WriteLine("Insert x0 in following line");
             Double.TryParse(Console.ReadLine().Replace('.', ','), out x0);
             Thread.Sleep(1);
-            velocprev = vel0;
-            deltat = 1e-16;
+            velocprev = 0;
+            deltat = 0.1;
             xprev = x0;
             Console.WriteLine($"Here will be output of experimental data");
-            while (x>=0)
+            while (xprev>=0)
             {
-                accel = (mass * G - density * Math.Pow(velocprev, 2) * Surface * Multiplier / 2) / mass;
+                Console.WriteLine($"{t} , {deltat} , {x} , {veloc} , {accel} , {g} ");
+                accel = G - 0.5 * density * Math.Pow(velocprev, 2) * Surface * Multiplier / mass;
                 alpha = density * veloc * Surface * Multiplier * deltat;
                 g = 1 - alpha;
                 veloc = velocprev + accel * deltat;
                 x = xprev - velocprev * deltat - accel * Math.Pow(deltat, 2) / 2;
                 t = t + deltat;
-                deltat = mass / density / veloc / Surface / Multiplier * 0.2;
+                deltat = 0.2 * mass / (density * veloc * Surface * Multiplier);
                 xprev = x;
                 velocprev = veloc;
-                Console.WriteLine($"{t} , {deltat} , {x} , {veloc} , {accel} , {g} ");
+                
 
             }
             Console.ReadKey();
